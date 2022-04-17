d7a4fb66c4a545957c24ecbebc465fa305480874
vladd11 <rozhkov.2006@gmail.com>
2022-04-17 21:55:49 +0400

    Added filter for products. Based on MUI chips.

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,38 +1,44 @@
 import Image from "next/image";
 import {Info} from "./info"
 import Chip from "@mui/material/Chip";
-import {Typography} from "@mui/material";
-
+import {Button, Typography} from "@mui/material";
+import {AddShoppingCart} from "@mui/icons-material";
 
 function localLoader({src}) {
     return `https://site-deploy.pages.dev/${src}`;
 }
 
-function onChipClicked(index) {
-    return undefined;
-}
-
-export const Index = () => <main>
+const Index = ({onChipClicked, filteredProducts, clicked}) => <main>
     <div className="chips">
         {Info.categories.map(function (item, index) {
             return (
                 <Chip key={index} className="chip" label={<Typography component={"span"}>{item}</Typography>}
-                      onClick={onChipClicked(index)}></Chip>
+                      onClick={event => onChipClicked(item)}
+                      variant={(item === clicked) ? "filled" : "outlined"}></Chip>
             )
         })}
     </div>
     <div className="products">
-        {Info.products.map(function (item, index) {
+        {filteredProducts.map(function (item, index) {
             return (
                 <div key={index} className="product">
                     <div className="product_img"><Image src={item.image_uri} alt={'Пицца'} loader={localLoader}
-                                                        width='300'
-                                                        height='150'/></div>
+                                                        width="300"
+                                                        height="150"/></div>
                     <div className="product_title">{item.title}</div>
-                    <div className="product_description">{item.description}</div>
-                    <div className="product_price">{item.price / 100} ₽</div>
+                    <div className="sub">
+                        <div>
+                            <div className="product_description">{item.description}</div>
+                            <div className="product_price">{item.price / 100} ₽</div>
+                        </div>
+                        <Button variant="outlined" startIcon={<AddShoppingCart />} className="order_button">
+                            Заказать
+                        </Button>
+                    </div>
                 </div>
             )
         })}
     </div>
 </main>
+
+export {Index}
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,24 @@ import {Header} from '../lib/header'
 import {Footer} from '../lib/footer'
 import {Overlay} from '../lib/overlay'
 import {Index} from '../lib/index'
-
+import React from 'react'
 
 export default function Home() {
+    const [filteredProducts, setFilteredProducts] = React.useState(Info.products)
+    const [clicked, setClicked] = React.useState("")
+
+    const onChipClicked = (category) => {
+        if (category === clicked) {
+            setClicked("");
+            setFilteredProducts(Info.products);
+        } else {
+            setClicked(category)
+            setFilteredProducts(Info.products.filter(value => value.category === category));
+        }
+
+        return undefined;
+    }
+
     return (
         <div className="root">
             <Head>
@@ -18,7 +33,8 @@ export default function Home() {
             <Header></Header>
             <Overlay></Overlay>
 
-            <Index></Index>
+            <Index onChipClicked={(item) => onChipClicked(item)} filteredProducts={filteredProducts}
+                   clicked={clicked}></Index>
 
             <Footer/>
         </div>
